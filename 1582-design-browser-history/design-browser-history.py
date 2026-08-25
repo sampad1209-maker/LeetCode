165451d6564f9107d48e81736f99b6eaaee65cb6
sampad <sampad1209@gmail.com>
2026-08-25 10:01:38 +0530

    Time: 55 ms (57.34%) | Memory: 15.1 MB (48.46%) - LeetSync

---
+++ 1582-design-browser-history/design-browser-history.py
@@ -0,0 +1,21 @@
+class BrowserHistory:
+
+    def __init__(self, homepage):
+        self.history = [homepage]
+        self.current = 0
+
+    def visit(self, url):
+        # Remove all forward history
+        self.history = self.history[:self.current + 1]
+
+        # Add the new URL
+        self.history.append(url)
+        self.current += 1
+
+    def back(self, steps):
+        self.current = max(0, self.current - steps)
+        return self.history[self.current]
+
+    def forward(self, steps):
+        self.current = min(len(self.history) - 1, self.current + steps)
+        return self.history[self.current]

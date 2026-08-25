5b0a2ec26afa166bad3fa87789b1d5a8238066e0
sampad <sampad1209@gmail.com>
2026-08-25 10:14:15 +0530

    Time: 79 ms (29.21%) | Memory: 17.2 MB (90.06%) - LeetSync

---
+++ 969-number-of-recent-calls/number-of-recent-calls.py
@@ -0,0 +1,12 @@
+class RecentCounter:
+
+    def __init__(self):
+        self.requests = []
+
+    def ping(self, t):
+        self.requests.append(t)
+
+        while self.requests[0] < t - 3000:
+            self.requests.pop(0)
+
+        return len(self.requests)
